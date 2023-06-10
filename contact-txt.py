9c5e486e4dd920a5afc1ea9b05ff6ab08da8b71c
Douglas <douglasdeodato@gmail.com>
2023-06-10 23:38:18 +0100

    salvar em txt

--- contact-txt.py
+++ contact-txt.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('contact_form.html')
+    return render_template('form.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -12,10 +12,11 @@ def submit():
     email = request.form['email']
     telefone = request.form['telefone']
     mensagem = request.form['mensagem']
-    
-    # Do something with the form data (e.g., send an email, save to a database)
-    
-    return 'Form submitted successfully!'
+
+    with open("dados.txt", "a") as arquivo:
+        arquivo.write(f"{nome},{email},{telefone},{mensagem}\n")
+
+    return "Dados enviados com sucesso!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
